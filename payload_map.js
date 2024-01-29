43e7cd268f7565e53faff7e5bff3f24d02ff0749
Cubans Gamers <96275179+CubansGamers@users.noreply.github.com>
2024-01-29 02:01:41 -0500

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,13 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'Activar',
+            description: 'Ya Puedes Ejecutar Juegos y Aplicaciones.',
+            fileName: 'Activar.bin',
+            author: 'Cubans Gamers Host PS5',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0'
+        },
+    
+
+    ];
